356d008ffccf8d6a139ac6400d91f663c8cbda96
minnez <patrickackom003@gmail.com>
2022-09-10 23:33:57 +0000

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,46 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+    <div class="wrapper-main">
+        <nav>
+            <router-link to="/">Home</router-link>
+            <router-link to="/lyrics">Lyrics</router-link>
+            <router-link to="/album">Album</router-link>
+            <router-link to="/artist">Artist</router-link>
+            <router-link to="/my-songs">My songs</router-link>
+        </nav>
+        <main>
+            <router-view />
+        </main>
+    </div>
 </template>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+    font-family: sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    height: 100vh;
 }
-
-nav {
-  padding: 30px;
+.wrapper-main {
+    display: grid;
+    grid-template-columns: 200px auto;
+    height: 100%;
+    outline: 1px solid red;
+}
+.wrapper-main nav {
+    display: flex;
+    flex-direction: column;
+    outline: 1px solid #8f0fc5;
+}
+.wrapper-main main {
+    outline: 1px solid blue;
+    height: 100%;
 }
 
 nav a {
-  font-weight: bold;
-  color: #2c3e50;
+    font-weight: bold;
 }
 
 nav a.router-link-exact-active {
-  color: #42b983;
+    color: #8f0fc5;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,39 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
+    {
+        path: "/",
+        name: "home",
+        component: HomeView,
+    },
+    {
+        path: "/lyrics",
+        name: "lyrics",
+        component: () =>
+            import(/* webpackChunkName: "about" */ "../views/Lyrics.vue"),
+    },
+    {
+        path: "/album",
+        name: "album",
+        component: () => import("../views/Album.vue"),
+    },
+    {
+        path: "/artist",
+        name: "artist",
+        component: () => import("../views/Artist.vue"),
+    },
+    {
+        path: "/my-songs",
+        name: "mysongs",
+        component: () => import("../views/Mysongs.vue"),
+    },
+];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+    history: createWebHistory(process.env.BASE_URL),
+    routes,
+    linkActiveClass: "active",
+});
 
-export default router
+export default router;
