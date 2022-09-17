0edc0881c1ec9463f4f4e64fbb142c39e67e72d9
minnez <patrickackom003@gmail.com>
2022-09-17 09:51:54 +0000

    created the loading page

--- src/views/Lyrics.vue
+++ src/views/Lyrics.vue
@@ -1,129 +1,14 @@
 <template>
     <main>
-        <div class="lyrics-main">
-            <SongBar
-                albumCover="album1.jpg"
-                songTitle="Final Countdown"
-                artistName="Paa Bobo"
-                duration="3:16"
-            />
-            <SongBar
-                albumCover="album3.jpg"
-                songTitle="River Flows in You"
-                artistName="Yiruma"
-                duration="6:03"
-            />
-            <SongBar
-                albumCover="album2.jpg"
-                songTitle="Funky Friday"
-                artistName="Ohemaa Mercy"
-                duration="3:26"
-            />
-            <SongBar
-                albumCover="album4.jpg"
-                songTitle="Runaway"
-                artistName="21 Savage"
-                duration="4:46"
-            />
-            <SongBar
-                albumCover="album1.jpg"
-                songTitle="Despacito"
-                artistName="Michael Jackson"
-                duration="5:46"
-            />
-            <SongBar
-                albumCover="album1.jpg"
-                songTitle="Final Countdown"
-                artistName="Paa Bobo"
-                duration="3:16"
-            />
-            <SongBar
-                albumCover="album3.jpg"
-                songTitle="River Flows in You"
-                artistName="Yiruma"
-                duration="6:03"
-            />
-            <SongBar
-                albumCover="album2.jpg"
-                songTitle="Funky Friday"
-                artistName="Ohemaa Mercy"
-                duration="3:26"
-            />
-            <SongBar
-                albumCover="album4.jpg"
-                songTitle="Runaway"
-                artistName="21 Savage"
-                duration="4:46"
-            />
-            <SongBar
-                albumCover="album1.jpg"
-                songTitle="Despacito"
-                artistName="Michael Jackson"
-                duration="5:46"
-            />
-            <SongBar
-                albumCover="album1.jpg"
-                songTitle="Final Countdown"
-                artistName="Paa Bobo"
-                duration="3:16"
-            />
-            <SongBar
-                albumCover="album3.jpg"
-                songTitle="River Flows in You"
-                artistName="Yiruma"
-                duration="6:03"
-            />
-            <SongBar
-                albumCover="album2.jpg"
-                songTitle="Funky Friday"
-                artistName="Ohemaa Mercy"
-                duration="3:26"
-            />
-            <SongBar
-                albumCover="album4.jpg"
-                songTitle="Runaway"
-                artistName="21 Savage"
-                duration="4:46"
-            />
-            <SongBar
-                albumCover="album1.jpg"
-                songTitle="Despacito"
-                artistName="Michael Jackson"
-                duration="5:46"
-            />
-            <SongBar
-                albumCover="album3.jpg"
-                songTitle="River Flows in You"
-                artistName="Yiruma"
-                duration="6:03"
-            />
-            <SongBar
-                albumCover="album2.jpg"
-                songTitle="Funky Friday"
-                artistName="Ohemaa Mercy"
-                duration="3:26"
-            />
-            <SongBar
-                albumCover="album4.jpg"
-                songTitle="Runaway"
-                artistName="21 Savage"
-                duration="4:46"
-            />
-            <SongBar
-                albumCover="album1.jpg"
-                songTitle="Despacito"
-                artistName="Michael Jackson"
-                duration="5:46"
-            />
-        </div>
+        <Loading />
     </main>
 </template>
 
 <script>
-import SongBar from "@/components/SongBar.vue";
+import Loading from "@/components/loading/Loading.vue";
 export default {
     components: {
-        SongBar,
+        Loading,
     },
 };
 </script>
